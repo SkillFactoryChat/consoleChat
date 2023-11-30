7ca4161b01445698033548a4aef9f281b7554b64
slobodskov <148288578+slobodskov@users.noreply.github.com>
2023-11-30 12:50:34 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	setlocale(LC_ALL, "");
-	Chat chat;
-	chat.Start();
-	while (chat.ChatWorkCheck())
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+	Chat messenger;
+	messenger.Start();
+	while (messenger.ChatWorkCheck())
 	{
-		chat.ShowLoginMenu();
-		while (chat.GetCurrentUser()) {
-			chat.ShowUserMenu();
+		messenger.ShowLoginMenu();
+		while (messenger.GetCurrentUser()) {
+			messenger.ShowUserMenu();
 		}
 	}
 	return 0;
